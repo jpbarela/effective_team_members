8cfa94f5c167f657d43f45148c51448a8ccd5972
John Barela <jbarela@commercialtribe.com>
2017-01-26 16:54:35 -0700

    Add assignment model.

---
+++ models/assignment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AssignmentSchema = Schema({
+  name: { type: String },
+  score: { type: Number, min: 0, max: 100 },
+  studentId: { type: Schema.Types.ObjectId, ref: 'Student' }
+});
+
+module.exports = mongoose.model('Assignment', AssignmentSchema);
