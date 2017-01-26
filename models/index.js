bcd75423a3444103cedd701394acb422a3da99f6
John Barela <jbarela@commercialtribe.com>
2017-01-26 10:34:42 -0700

    Add student model

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const config = require('../config');
+
+const student = require('./student');
+
+mongoose.Promise = global.Promise;
+const connection = mongoose.connect(config.db);
+
+module.exports = {
+	connection,
+	student
+};
