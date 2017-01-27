a6d9d1437d4404f22903157d1375bed6a9f66a06
John Barela <jbarela@commercialtribe.com>
2017-01-26 18:37:18 -0700

    Fix eslint errors.

--- controllers/meanScores.js
+++ controllers/meanScores.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const assignmentModel = require('../models/assignment');
+
 const router = express.Router();
 
 router.get('/students/:studentId/meanScore', (req, res) => {
   assignmentModel.find({ studentId: req.params.studentId }, (err, assignments) => {
     let scoreSum = 0;
-    for(i = 0; i < assignments.length; i++) {
-      scoreSum += assignments[i].score
+    for (let i = 0; i < assignments.length; i++) {
+      scoreSum += assignments[i].score;
     }
-    const average = assignments.length ? scoreSum/ assignments.length : null;
-    res.json({ meanScore: average })
-  })
+    const average = assignments.length ? scoreSum / assignments.length : null;
+    res.json({ meanScore: average });
+  });
 });
 
 module.exports = router;
