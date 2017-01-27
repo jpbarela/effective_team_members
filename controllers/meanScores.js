a6c0056fddc14bcd1366728825c7170b3e457ce7
John Barela <jbarela@commercialtribe.com>
2017-01-26 18:33:18 -0700

    Add meanScores route

---
+++ controllers/meanScores.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const assignmentModel = require('../models/assignment');
+const router = express.Router();
+
+router.get('/students/:studentId/meanScore', (req, res) => {
+  assignmentModel.find({ studentId: req.params.studentId }, (err, assignments) => {
+    let scoreSum = 0;
+    for(i = 0; i < assignments.length; i++) {
+      scoreSum += assignments[i].score
+    }
+    const average = assignments.length ? scoreSum/ assignments.length : null;
+    res.json({ meanScore: average })
+  })
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const routes = express.Router();
 
 const students = require('./studentsRoutes');
+const meanScoreController = require('../controllers/meanScores');
 
 routes.use('/students', students);
+routes.use('/', meanScoreController);
 
 module.exports = routes;
